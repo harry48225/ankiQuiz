78dcdd9189b2c5ea6787a1223bfade134542c3e0
Harry <harrybest800@gmail.com>
2019-07-01 18:34:22 +0100

    Visually implemented other answers

--- __init__.py
+++ __init__.py
@@ -5,6 +5,8 @@ from aqt.utils import showInfo
 # import all of the Qt GUI library
 from aqt.qt import *
 
+from random import shuffle
+
 # We're going to add a menu item below. First we want to create a function to
 # be called when the menu item is activated.
 
@@ -29,28 +31,53 @@ def quiz():
 
     cards = getReviews()
     
-    mw.myWidget = widget = QuizQuestion(cards[0])
+    mw.myWidget = widget = QuizQuestion(cards[0], cards[1:4])
     widget.show()
     
 
 class QuizQuestion(QWidget):
 
-    def __init__(self, cardid):
+    def __init__(self, cardid, othercards):
         super(QuizQuestion, self).__init__()
 
         self.cardid = cardid
-        self.card = mw.col.getCard(cardid)
+        self.card = mw.col.getCard(cardid) #
+        self.othercards = self.getothers(othercards) # These are the cards whose answers will also be shown
 
         self.doLayout()
 
+    def getothers(self, otherids):
+        others = []
+        for cid in otherids:
+            others.append(mw.col.getCard(cid)) # Get the cards coressponding to the ids
+
+        return others
+
     def doLayout(self):
         
-        layout = QVBoxLayout()
-        note = self.card.note()
-        layout.addWidget(QLabel(note['Front']))
-        layout.addWidget(QLabel(note['Back']))
-        self.setLayout(layout)
+        layout = QHBoxLayout()
+        realnote = self.card.note()
+        
+        
+        # Prepare the answer section
+        answerssection = QWidget()
+        answers = QVBoxLayout()
+        
+        allnotes = [c.note() for c in self.othercards] # Get the notes to do with the other cards
+        allnotes.append(realnote)
+        shuffle(allnotes) # Shuffle them
 
+        for note in allnotes:
+            answers.addWidget(QRadioButton(note['Back']))
+        #answers.addWidget(QLabel(note['Back']))
+
+
+        answerssection.setLayout(answers)
+
+        layout.addWidget(QLabel(realnote['Front']))
+        layout.addWidget(answerssection)
+        
+        self.setLayout(layout)
 # create a new menu item, "test"
 action = QAction("doQuiz", mw)
 # set it to call testFunction when it's clicked
